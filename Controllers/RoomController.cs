2d70218b06cf4d1215043c977dca48af7ce64f98
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-19 20:53:53 -0700

    Add files via upload

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdventureExplore.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Rooms(string name, int width=5)
+        {
+            return HtmlEncoder.Default.Encode($"{name} Room, Width is : {width}");
+        }
+    }
+}
