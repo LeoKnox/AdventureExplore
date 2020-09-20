c1831161410812418a9373150ebd51c4d8d432fe
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-20 14:13:09 -0700

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdventureExplore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureExplore.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public string Description { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public virtual ICollection<Door> Doors { get; set; }
+    }
+
+    public class Door
+    {
+        public int DoorId { get; set; }
+        public string Material { get; set; }
+        public int Wall { get; set; }
+        public int Position { get; set; }
+    }
+}
