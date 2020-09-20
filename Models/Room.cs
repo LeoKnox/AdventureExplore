534578da6009221389ed63e30783d3e704ba5087
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-20 15:00:10 -0700

    Add files via upload

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,8 @@ namespace AdventureExplore.Models
 {
     public class Room
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string RoomName { get; set; }
         public string Description { get; set; }
@@ -17,6 +21,8 @@ namespace AdventureExplore.Models
 
     public class Door
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int DoorId { get; set; }
         public string Material { get; set; }
         public int Wall { get; set; }
