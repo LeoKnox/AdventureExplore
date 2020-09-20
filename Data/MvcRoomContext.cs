2911cc9046bad633d26ef8bd612e880ecaf8719b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-20 15:09:07 -0700

    Add files via upload

---
+++ Data/MvcRoomContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AdventureExplore.Models;
+
+namespace AdventureExplore.Data
+{
+    public class MvcRoomContext : DbContext
+    {
+        public MvcRoomContext (DbContextOptions<MvcRoomContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Room> Room { get; set; }
+    }
+}
